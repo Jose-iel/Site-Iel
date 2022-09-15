203e8fab119f922a1c13a4260ed8fb46c153b60f
José <josehenriqueiel@gmail.com>
2022-09-15 09:48:22 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     let containerA = document.getElementById("circleA");
 
     let circleA = new ProgressBar.Circle(containerA, {
-        color: '#64DAF9',
+        color: '#EC9732',
         strokeWidth: 8,
         duration: 1400,
         from: { color: '#AAA'},
-        to: { color: '#65DAF9'},
+        to: { color: '#EC9732'},
 
         step: function(state, circle){
             circle.path.setAttribute("stroke", state.color);
@@ -21,11 +21,11 @@ $(document).ready(function() {
     let containerB = document.getElementById("circleB");
 
     let circleB = new ProgressBar.Circle(containerB, {
-        color: '#64DAF9',
+        color: '#EC9732',
         strokeWidth: 8,
         duration: 1600,
         from: { color: '#AAA'},
-        to: { color: '#65DAF9'},
+        to: { color: '#EC9732'},
 
         step: function(state, circle){
             circle.path.setAttribute("stroke", state.color);
@@ -39,11 +39,11 @@ $(document).ready(function() {
     let containerC = document.getElementById("circleC");
 
     let circleC = new ProgressBar.Circle(containerC, {
-        color: '#64DAF9',
+        color: '#EC9732',
         strokeWidth: 8,
         duration: 2000,
         from: { color: '#AAA'},
-        to: { color: '#65DAF9'},
+        to: { color: '#EC9732'},
 
         step: function(state, circle){
             circle.path.setAttribute("stroke", state.color);
@@ -57,11 +57,11 @@ $(document).ready(function() {
     let containerD = document.getElementById("circleD");
 
     let circleD = new ProgressBar.Circle(containerD, {
-        color: '#64DAF9',
+        color: '#EC9732',
         strokeWidth: 8,
         duration: 2200,
         from: { color: '#AAA'},
-        to: { color: '#65DAF9'},
+        to: { color: '#EC9732'},
 
         step: function(state, circle){
             circle.path.setAttribute("stroke", state.color);
